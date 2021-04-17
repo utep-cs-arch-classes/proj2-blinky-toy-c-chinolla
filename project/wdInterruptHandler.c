2a6de64d4809d9ada5234456325156e4977eef5a
Cristobal Chinolla <c.chinolla@ttu.edu>
2021-04-16 21:47:29 -0600

    finished wdInterruptHandler

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -1,11 +1,18 @@
 #include <msp430.h>
 #include "stateMachines.h"
-
+#include "buzzer.h"
+#include "switches.h"
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 125) {
+  if (state = 3 && ++blink_count == 1)
+  {
     state_advance();
     blink_count = 0;
   }
+  else if (++blink_count == 250)
+    {
+      state_advance();
+      blink_count = 0;
+    }
 }
