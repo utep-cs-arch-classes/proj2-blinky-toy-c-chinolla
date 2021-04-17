0cb44be6983dec0cd29bee517e8ac7f0afd3c0d5
Cristobal Chinolla <c.chinolla@ttu.edu>
2021-04-16 22:44:57 -0600

    added dim functions (one, two, and three) and updated the state_advance() function

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -1,6 +1,8 @@
 #include <msp430.h>
 #include "stateMachines.h"
 #include "led.h"
+#include "switches.h"
+#include "buzzer.h"
 
 char toggle_red()		
 {
@@ -20,12 +22,19 @@ char toggle_red()
 
 char toggle_green()	/* only toggle green if red is on!  */
 {
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
+  static char state = 0;
+  switch(state)
+    {
+    case 0:
+      green_on = 1;
+      state = 1;
+      break;
+    case 1:
+      green_on = 0;
+      state = 0;
+      break;
+    }
+  return 1;
 }
 
 
@@ -34,14 +43,170 @@ void state_advance()		/* alternate between toggling red & green */
   char changed = 0;  
 
   static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
-  }
+  switch (s)
+    {
+    case 0:
+      red_on = 1;
+      green_on = 1;
+      changed = 1;
+      break;
+    case 1:
+      testdimLights();
+      changed = 1;
+      break;
+    case 2:
+      changed = toggle_both();
+      makeSound();
+      break;
+    case 3:
+      changed = turn_off();
+      buzzer_set_period(0);
+      break;
+    }
+     
 
   led_changed = changed;
   led_update();
 }
 
 
+char toggle_both()
+{
+  static char state = 0;
 
+  switch(state)
+    {
+    case 0:
+      red_on = 1;
+      green_on = 0;
+      state = 1;
+      break;
+    case 1:
+      red_on = 0;
+      green_on = 1;
+      state = 0;
+      break;
+    }
+  return 1;
+}
+
+void dimone()
+{
+  char changed_led = 0;
+  static char dimS = 0;
+  switch(dimS)
+    {
+    case 0:
+      red_on = 1;
+      dimS = 1;
+      break;
+    case 1:
+      red_on = 0;
+      dimS = 2;
+      break;
+    case 2:
+      red_on = 0;
+      dimS = 3;
+      break;
+    case 3:
+      red_on = 0;
+      dimS = 0;
+      break;
+    }
+  changed_led = 1;
+  led_update();
+}
+
+
+void dimtwo()
+{
+  char changed_led = 0;
+  static char dimS = 0;
+  switch(dimS)
+    {
+    case 0:
+      red_on = 1;
+      dimS = 1;
+      break;
+    case 1:
+      red_on = 0;
+      dimS = 2:
+      break;
+    case 2:
+      red_on = 1:
+      dimS = 3;
+      break;
+    case 3:
+      red_on = 0;
+      dimS = 0;
+      break;
+    }
+  changed_led = 1;
+  led_update();
+}
+
+void dimthree()
+{
+  char changed_led = 0;
+  static char dimS = 0;
+  switch(dimS)
+    {
+    case 0:
+      red_on = 1:
+      dimS = 1;
+      break;
+    case 1:
+      red_on = 0;
+      dimS = 2;
+      break;
+    case 2:
+      red_on = 1;
+      dimS = 3;
+      break;
+    case 3:
+      red_on = 1;
+      dimS = 0;
+      break;
+    }
+  changed_led = 1;
+  led_update();
+}
+
+void dimLights()
+{
+  static char dimS = 0;
+  switch(dimS)
+    {
+      dimone();
+      dimS = 1;
+      break;
+    case 1:
+      dimtwo();
+      dimS = 2;
+      break;
+    case 2:
+      dimthree();
+      dimS = 3;
+      break;
+    }
+}
+
+void testdimLights()
+{
+  static char state = 0;
+  switch(state)
+    {
+    case 0:
+      red_on = 1;
+      green_on = 1:
+      state = 1;
+      break;
+    case 1:
+      red_on = 0;
+      green_on = 0;
+      state = 0;
+      break;
+    }
+  led_changed = 1;
+  led_update();
+}
