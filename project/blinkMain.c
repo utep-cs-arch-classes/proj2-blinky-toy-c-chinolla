de61ecfcc1875e22a1f4b16e2023662cf85e5b32
Cristobal Chinolla <c.chinolla@ttu.edu>
2021-04-16 23:14:49 -0600

    added initializations for switches and buzzer

--- project/blinkMain.c
+++ project/blinkMain.c
@@ -2,11 +2,15 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"
+#include "buzzer.h"
+#include "switches.h"
+
 
 int main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   led_init();
-  enableWDTInterrupts();	/* enable periodic interrupt */
-
+  enableWDTInterrupts();/* enable periodic interrupt */
+  switch_init();
+  buzzer_init();
   or_sr(0x18);		/* CPU off, GIE on */
 }
